9949ed13648adbcbbe2129fdf344cc5ef1b54b28
Ethionathan <natnaelak@gmail.com>
2022-07-01 13:51:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,19 @@
-#!/bin/bash
-gcc $CFILE -o cisfun
+#include <stdio.h>
+
+/**
+ *
+ *  * Description: main - prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
