fc1410e82aadea7d1400fb5fb7b2943aafeafc48
Ethionathan <natnaelak@gmail.com>
2022-07-01 11:14:27 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * return: Always 0 (Success)
- */
+ *  * Description: main - prints ouy a string to stdout.
+ *   * Return: 0 if success.
+ *    */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
